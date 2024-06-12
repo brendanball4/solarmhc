36165d44c1c79b87e04bbf70e0c1a05f08bd8611
Brendan Ball <Brendan.Ball@infotech.pri>
2024-06-12 15:06:29 -0600

    Created models for the database structure. Created components for modularity.

---
+++ WebScraper/Models/InverterType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper.Models
+{
+    internal class InverterType
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal Capacity { get; set; } // in kW
+    }
+}
---
+++ WebScraper/Models/PowerIntake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper.Models
+{
+    internal class PowerIntake
+    {
+        public int Id { get; set; }
+        public SolarArray ArrayName { get; set; }
+        public decimal KW { get; set; }
+        public int Utilization { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
