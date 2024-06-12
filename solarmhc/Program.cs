c9f513c9168fc558a6dc9d85e21276eedfe69003
brendanball4 <ball.brendan10@gmail.com>
2024-06-11 19:18:48 -0600

    Styling and charting"

--- solarmhc/Program.cs
+++ solarmhc/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using solarmhc.Data;
 using WebScraper;
 using MudBlazor.Services;
 
@@ -9,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddHttpClient<FroniusWebScraper>();
 builder.Services.AddMudServices();
 
