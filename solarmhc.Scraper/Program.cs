1a7fc369c59f32b8dedcc4c42aa23ede4be3dcbb
brendanball4 <ball.brendan10@gmail.com>
2024-08-21 23:34:39 -0600

    Installed hosting service for scraping

--- solarmhc.Scraper/Program.cs
+++ solarmhc.Scraper/Program.cs
@@ -1,21 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using solarmhc.Scraper.Services;
 using solarmhc.Scraper;
 using solarmhc.Scraper.Data;
-using solarmhc.Scraper.Services;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService()
     .ConfigureServices((context, services) =>
     {
-
         services.AddDbContext<SolarMHCDbContext>(options =>
             options.UseSqlServer(context.Configuration.GetConnectionString("DevSolarMhcDatabase")));
-
 
         services.AddHostedService<Worker>();
         services.AddSingleton<ChromeDriverManager>();
         services.AddSingleton<WebScraperHelperService>();
         services.AddHttpClient<WebScraperService>();
         services.AddScoped<WebScraperService>();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
     })
     .Build();
 
