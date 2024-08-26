38e1fecb18a098ae76f6692848ee7f2f6f8a52ac
brendanball4 <ball.brendan10@gmail.com>
2024-08-26 15:40:54 -0600

    Updated gitignore

--- WebScraper/obj/Debug/net6.0/solarmhc.Models.AssemblyInfo.cs
+++ WebScraper/obj/Debug/net6.0/solarmhc.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("solarmhc.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3059fed90458534b266001a6f5e5dafa0c28b313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("solarmhc.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("solarmhc.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
